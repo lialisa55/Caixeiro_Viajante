0f30283024f9905e8eaf5d40ae170ffe41a4bc78
mona <119463033+lialisa55@users.noreply.github.com>
2024-10-10 20:49:06 -0300

    i need you like a need a gaping head wound

--- finalizado/main.c
+++ finalizado/main.c
@@ -4,32 +4,34 @@
 #include <stdbool.h>
 #include "garfo.h"
 #define min(a,b) (a<b ? a:b)
-//esta funcao testa recursivamente todo caminho possivel com as conexoes dadas, salvando o melhor caminho em path e a distancia em best
-int my_recursion(GARFO *garfo, int visitados[12], int path[12], int cidade, int destino, int dist, int cnt, int n, int *best){
-    visitados[cidade] = true;
-    for (int i = 0; i < garfo_get_size_connections(garfo, cidade); i++){
+
+int encontrar_menor_caminho(GARFO *garfo, int visitados[12], int path[12], int atual, int destino, int dist, int num_visitadas, int num_cidades, int *best){
+    visitados[atual] = true;
+    for (int i = 0; i < garfo_get_size_connections(garfo, atual); i++){
         int prev = *best;
-        if (garfo_get_connection_destino(garfo,  cidade, i) == destino && cnt == n-1 && dist + garfo_get_connection_dist(garfo, cidade, i) < *best){
-            path[cnt+1] = destino;
-            *best = dist + garfo_get_connection_dist(garfo, cidade, i);
+        if (garfo_get_connection_destino(garfo, atual, i) == destino && num_visitadas == num_cidades-1 && dist + garfo_get_connection_dist(garfo, atual, i) < *best){
+            path[num_visitadas+1] = destino;
+            *best = dist + garfo_get_connection_dist(garfo, atual, i);
             break;
         }
-        if (!visitados[garfo_get_connection_destino(garfo,  cidade, i)]) my_recursion(garfo, visitados, path, garfo_get_connection_destino(garfo,  cidade, i), destino, dist + garfo_get_connection_dist(garfo, cidade, i), cnt + 1, n, best);
+        if (!visitados[garfo_get_connection_destino(garfo,  atual, i)]){
+            encontrar_menor_caminho(garfo, visitados, path, garfo_get_connection_destino(garfo,  atual, i), destino, dist + garfo_get_connection_dist(garfo, atual, i), num_visitadas + 1, num_cidades, best);
+        }
         if (*best < prev){
-            path[cnt+1] = garfo_get_connection_destino(garfo,  cidade, i);
+            path[num_visitadas+1] = garfo_get_connection_destino(garfo,  atual, i);
         }
     }
-    visitados[cidade] = false;
+    visitados[atual] = false;
     return *best;
 }
 
 int main(){
-    int n, comeco, k;
-    scanf("%d %d %d", &n, &comeco, &k);
+    int num_cidades, comeco, num_conexoes;
+    scanf("%d %d %d", &num_cidades, &comeco, &num_conexoes);
     comeco--;
-    GARFO *garfo = garfo_criar(n);
+    GARFO *garfo = garfo_criar(num_cidades);
     int a, b, dist;
-    while (k--){
+    while (num_conexoes--){
         scanf("%d %d %d", &a, &b, &dist);
         a--; b--;
         garfo_add_connection(garfo, a, b, dist);
@@ -38,10 +40,10 @@ int main(){
     int visitados[12] = {0};
     int path[13] = {comeco, -1, -1, -1, -1, -1, -1, -1, -1};
     int best = 1000000;
-    best = my_recursion(garfo, visitados, path,  comeco, comeco, 0, 0, n, &best);
-    for (int i = 0; i <= n; i++){
+    best = encontrar_menor_caminho(garfo, visitados, path,  comeco, comeco, 0, 0, num_cidades, &best);
+    for (int i = 0; i <= num_cidades; i++){
         printf("%d ", path[i]+1);
     }
-    printf("%d\n", best);
+    printf("%d\num_cidades", best);
     garfo_apagar(&garfo);
 }
